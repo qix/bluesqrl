cf1dcb276101f1f627852ed78c61e15018b2e221
Josh Yudaken <j@yud.co.za>
2023-05-12 21:22:03 -0400

    Add https://github.com/moeenz/docker-kafka-kraft as ops/dev-kafka

---
+++ docker/dev-kafka/docker-entrypoint.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+_term() {
+    echo "🚨 Termination signal received...";
+    kill -TERM "$child" 2>/dev/null
+}
+
+trap _term SIGINT SIGTERM
+
+properties_file=/opt/kafka/config/kraft/server.properties;
+kafka_addr=localhost:9093;
+
+echo "==> Applying environment variables...";
+
+
+if [ -z $KRAFT_CONTAINER_HOST_NAME ]; then
+    echo "listeners=CONTROLLER://:19092,EXTERNAL://:9093" >> $properties_file;
+    echo "advertised.listeners=EXTERNAL://localhost:9093" >> $properties_file;
+    echo "inter.broker.listener.name=EXTERNAL" >> $properties_file;
+    echo "listener.security.protocol.map=CONTROLLER:PLAINTEXT,EXTERNAL:PLAINTEXT" >> $properties_file;
+else
+    echo "listeners=CONTROLLER://:19092,INTERNAL://:9092,EXTERNAL://:9093" >> $properties_file;
+    echo "advertised.listeners=INTERNAL://${KRAFT_CONTAINER_HOST_NAME}:9092,EXTERNAL://localhost:9093" >> $properties_file;
+    echo "inter.broker.listener.name=EXTERNAL" >> $properties_file;
+    echo "listener.security.protocol.map=CONTROLLER:PLAINTEXT,INTERNAL:PLAINTEXT,EXTERNAL:PLAINTEXT" >> $properties_file;
+fi
+echo "==> ✅ Enivronment variables applied.";
+
+
+echo "==> Setting up Kafka storage...";
+export suuid=$(./bin/kafka-storage.sh random-uuid);
+./bin/kafka-storage.sh format -t $suuid -c ./config/kraft/server.properties;
+echo "==> ✅ Kafka storage setup.";
+
+
+echo "==> Starting Kafka server...";
+./bin/kafka-server-start.sh ./config/kraft/server.properties &
+child=$!
+echo "==> ✅ Kafka server started.";
+
+if [ -z $KRAFT_CREATE_TOPICS ]; then
+    echo "==> No topic requested for creation.";
+else
+    echo "==> Creating topics...";
+    ./wait-for-it.sh $kafka_addr;
+
+    pc=1
+    if [ $KRAFT_PARTITIONS_PER_TOPIC ]; then
+        pc=$KRAFT_PARTITIONS_PER_TOPIC
+    fi
+
+    for i in $(echo $KRAFT_CREATE_TOPICS | sed "s/,/ /g")
+    do
+        ./bin/kafka-topics.sh --create --topic "$i" --partitions "$pc" --replication-factor 1 --bootstrap-server $kafka_addr;
+    done
+    echo "==> ✅ Requested topics created.";
+fi
+
+
+wait "$child";
